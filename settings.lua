9e7dce8c78b59388f22ba678b2df74a1824e1c7c
TheTaurunti <153017348+TheTaurunti@users.noreply.github.com>
2024-01-29 19:37:08 -0800

    initial commit

---
+++ settings.lua
@@ -0,0 +1,16 @@
+data:extend({
+  {
+    type = "int-setting",
+    name = "UndergroundLength-belt-length",
+    setting_type = "startup",
+    default_value = 4,
+    minimum_value = 1
+  },
+  {
+    type = "int-setting",
+    name = "UndergroundLength-pipe-length",
+    setting_type = "startup",
+    default_value = 10,
+    minimum_value = 1
+  }
+})
