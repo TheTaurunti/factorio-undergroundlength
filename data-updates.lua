9fcebd8412edb5c160e4b7a0d11ed2cc812bc697
TheTaurunti <thetaurunti@gmail.com>
2025-03-29 19:11:10 -0700

    added more settings to customize how length scales

--- data-updates.lua
+++ data-updates.lua
@@ -1,4 +1,6 @@
-local belt_distance_per_15 = settings.startup["UndergroundLength-belt-length"].value
+local base_length = settings.startup["UndergroundLength-base-length"].value
+local belt_distance_per_15 = settings.startup["UndergroundLength-distance-per-15"].value
+local bus_spacing = settings.startup["UndergroundLength-add-spacing-for-bus"].value
 local pipe_distance = settings.startup["UndergroundLength-pipe-length"].value
 
 -- Belts
@@ -11,7 +13,8 @@ for _, belt in pairs(undergrounds) do
   -- >> This is so that the values are super nice in the context of a main bus.
   -- >> Equation ends with "-1" because the belt needs 1 more tile to connect to the end point.
   -- ... so to have 4 spaces in between it needs length of 5.
-  local new_length = (belt_distance_per_15 + 2) * math.max(1, fifteens) - 1
+  local belt_spacing_for_bus = bus_spacing and 2 or 0
+  local new_length = 1 + base_length + (belt_distance_per_15 + belt_spacing_for_bus) * math.max(1, fifteens)
 
   belt.max_distance = new_length
 end
@@ -21,6 +24,7 @@ local undergound_pipes = data.raw["pipe-to-ground"]
 for _, pipe in pairs(undergound_pipes) do
   if (pipe.fluid_box.pipe_connections[2])
   then
-    pipe.fluid_box.pipe_connections[2].max_underground_distance = pipe_distance
+    -- +1 is added so it SPANS the given length. As in, the length given is the number of spaces between underground pipes
+    pipe.fluid_box.pipe_connections[2].max_underground_distance = pipe_distance + 1
   end
 end
--- settings.lua
+++ settings.lua
@@ -1,16 +1,33 @@
 data:extend({
   {
     type = "int-setting",
-    name = "UndergroundLength-belt-length",
+    name = "UndergroundLength-base-length",
     setting_type = "startup",
-    default_value = 4,
-    minimum_value = 1
+    default_value = 2,
+    minimum_value = 1,
+    order = "1"
+  },
+  {
+    type = "int-setting",
+    name = "UndergroundLength-distance-per-15",
+    setting_type = "startup",
+    default_value = 2,
+    minimum_value = 1,
+    order = "2"
+  },
+  {
+    type = "bool-setting",
+    name = "UndergroundLength-add-spacing-for-bus",
+    setting_type = "startup",
+    default_value = false,
+    order = "3"
   },
   {
     type = "int-setting",
     name = "UndergroundLength-pipe-length",
     setting_type = "startup",
     default_value = 10,
-    minimum_value = 1
+    minimum_value = 1,
+    order = "4"
   }
 })
